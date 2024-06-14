1c0bdc2e07819ac3563ad5907b095f2a26715e33
Alex Walton <alex.w.walton@gmail.com>
2024-06-14 09:09:39 +0100

    feat(day-09): add day-09 part 1 and part 2 solutions

---
+++ day-09/src/bin/part1.rs
@@ -0,0 +1,65 @@
+fn main() {
+    let input = include_str!("input.txt");
+    let result = part1(input);
+
+    println!("Result: {}", result);
+}
+
+fn part1(input: &str) -> i64 {
+    let sequences: Vec<Vec<i64>> = input
+        .lines()
+        .map(|l| l.split_whitespace().flat_map(|n| n.parse()).collect())
+        .collect();
+
+    sequences.iter().map(|seq| next_number(seq)).sum()
+}
+
+fn next_number(sequence: &Vec<i64>) -> i64 {
+    let steps = steps(sequence);
+    if steps.iter().all(|n| *n == steps[0]) {
+        sequence.last().unwrap() + steps[0]
+    } else {
+        let next_step = next_number(&steps);
+        sequence.last().unwrap() + next_step
+    }
+}
+
+fn steps(sequence: &Vec<i64>) -> Vec<i64> {
+    let mut sequence = sequence.iter();
+    let mut last = sequence.next().unwrap();
+
+    sequence
+        .map(|n| {
+            let next = n - last;
+            last = n;
+            next
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_steps() {
+        assert_eq!(steps(&vec![1, 2, 3]), vec![1, 1]);
+        assert_eq!(steps(&vec![1, 3, 6, 10]), vec![2, 3, 4]);
+    }
+
+    #[test]
+    fn test_next_number() {
+        assert_eq!(next_number(&vec![0, 0, 0, 0]), 0);
+        assert_eq!(next_number(&vec![1, 1, 1, 1]), 1);
+        assert_eq!(next_number(&vec![1, 2, 3, 4]), 5);
+        assert_eq!(next_number(&vec![2, 4, 6, 8]), 10);
+        assert_eq!(next_number(&vec![1, 3, 6, 10]), 15);
+        assert_eq!(next_number(&vec![10, 13, 16, 21, 30, 45]), 68);
+    }
+
+    #[test]
+    fn test_part1() {
+        let input = include_str!("test1.txt");
+        assert_eq!(part1(input), 114);
+    }
+}
